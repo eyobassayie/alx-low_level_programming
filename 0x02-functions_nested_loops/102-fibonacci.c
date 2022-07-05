c47d4a892261535b12c87413c8e5cac63c9234fa
eyob <85004717+eyobassayie@users.noreply.github.com>
2022-07-05 16:39:18 +0300

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,31 +1,13 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include "main.h"
 /**
  * main - Entry point
- * Description: Calculates the first 50 Fibonacci numbers
- * Return: Always Zero
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	long before, after;
-	int count = 0;
 
-	before = 1;
-	after = 2;
-	printf("%ld, ", before);
-	printf("%ld, ", after);
-		while (count <= 47)
-		{
-			long fibonnaci;
-			fibonnaci = before + after;
-			printf("%ld", fibonnaci);
-			if (count != 47)
-				printf(", ");
-			before = after;
-			after = fibonnaci;
-			count++;
-		}
-	printf("\n");
 	return (0);
 }
