c7714b47f28b6c949ee246eb211a8eb72480b654
eyob <85004717+eyobassayie@users.noreply.github.com>
2022-07-07 11:43:36 +0300

    Update 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,8 +11,7 @@
 */
 void print_remaining_days(int month, int day, int year)
 {
-    if ((year % 4 == 0 || year % 400 == 0) ||
-    (year % 400 == 0 && year % 100 == 0 ))
+    if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0 ))
     {
         if (month >= 2 && day >= 60)
         {
@@ -33,10 +32,4 @@ void print_remaining_days(int month, int day, int year)
             printf("Day of the year: %d\n", day);
             printf("Remaining days: %d\n", 365 - day);
         }
-    }
-    int main(void)
-    {
-    int day = convert_day(04,01);
-    print_remaining_days(4,day,1997);
-    }
 }
