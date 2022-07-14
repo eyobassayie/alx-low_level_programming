d6bf48c3bc06ecbaf69d85f4709cb315550f183f
eyob <85004717+eyobassayie@users.noreply.github.com>
2022-07-14 21:04:17 +0300

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * infinite_add - adds two numbers
  * @n1: first number
@@ -8,6 +9,7 @@
  *
  * Return: address of r or 0
  */
+ 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 int i, j, k, l, m, n;
