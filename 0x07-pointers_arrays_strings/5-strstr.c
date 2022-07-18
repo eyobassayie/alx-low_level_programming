7295575f716cc1f359e009a0b40824a6250a8f7d
eyob <85004717+eyobassayie@users.noreply.github.com>
2022-07-18 08:06:07 +0300

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,31 +1,43 @@
-#include "holberton.h"
-/**
- *_strstr - find a string in a string.
- *@haystack: first string.
- *@needle: second string.
- *Return: char.
- */
+#include "main.h"
 
+/**
+ * _strncmp - compares two strings within b bytes.
+ * @s1: string to check
+ * @s2: string to check from
+ * @b: number of bytes to check
+ * Return: 0 if strings are the same
+ * any other number if they are different.
+ */
+ 
+int _strncmp(char *s1, char *s2, unsigned int b)
+{
+	unsigned int i;
+	for (i = 0; i <= b && *s1 == *s2 && *s1 != '\0' && *s2 != '\0'; i++)
+	{
+		s1++;
+		s2++;
+	}
+	return (i - b);
+}
+/**
+ * _strstr - locates a substring
+ * @haystack: string to look though
+ * @needle: substring to find
+ * Return: pointer to string with result of search
+ */
 char *_strstr(char *haystack, char *needle)
 {
-unsigned int length_needle = 0, i = 0, j, tmpi, checker;
-while (needle[length_needle] != '\0')
-length_needle++;
-while (haystack[i] != '\0')
-{
-tmpi = i;
-checker = 0;
-for (j = 0; j < length_needle; j++)
-{
-if (haystack[tmpi] == needle[j])
-{
-tmpi++;
-checker++;
-}
-}
-if (checker == length_needle)
-return (haystack + i);
-i++;
-}
-return (0);
+	unsigned int len;
+	/*Get length of needle for strncmp*/
+	len = 0;
+	while (needle[len] != '\0')
+		len++;
+	/*compare substring*/
+	while (*haystack != '\0')
+	{
+		if (_strncmp(haystack, needle, len) == 0)
+			return (haystack);
+		haystack++;
+	}
+	return (NULL);
 }
