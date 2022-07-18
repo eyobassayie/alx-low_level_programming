f5fcf58776311562c7ff9223468426f15c9274f8
eyob <85004717+eyobassayie@users.noreply.github.com>
2022-07-18 08:00:45 +0300

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include "main.h"
+
 /**
  * _memcpy - copies memory area
  * @dest: array of string
@@ -6,12 +8,12 @@
  * @n: number of bytes
  * Return: dest
  */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int i = 0;
-for (i = 0; i < n; i++)
-{
-dest[i] = src[i];
-}
-return (dest);
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
 }
