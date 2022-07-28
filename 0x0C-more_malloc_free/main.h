a468ef1adbfc60325691ee2aa67148c13e3751b1
eyob <85004717+eyobassayie@users.noreply.github.com>
2022-07-28 15:18:20 +0300

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,13 +2,10 @@
 #define HEADER_FILE
 
 int _putchar(char c);
-int _strlen(char *s);
-char *checkarg(char *s)
-char *makesecond(char *src, int l1,  char c, int zero);
-void *_calloc(unsigned int nmemb, unsigned int size);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-char *_mul(char *s1, char *s2);
-int main(int argc, char *argv[]);
+void _puts(char *str);
+int _atoi(const char *s);
+void print_int(unsigned long int n);
+int main(int argc, char const *argv[]);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
